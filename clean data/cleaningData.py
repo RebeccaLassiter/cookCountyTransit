091d077ae8d96ba35c2be333e964b01404337b57
Rebecca Lassiter <rebeccalassiter2020@u.northwestern.edu>
2019-12-05 08:32:16 -0600

    final pres

--- clean data/cleaningData.py
+++ clean data/cleaningData.py
@@ -14,6 +14,8 @@ total = tractData.iloc[:]["HC01_EST_VC01"]
 numTransit = tractData.iloc[:]["HC04_EST_VC01"]
 meanTravelTimeTransit = tractData.iloc[:]["HC04_EST_VC118"]
 medianIncome = tractData.iloc[:]["HC01_EST_VC51"]
+# leavingHome = tractData.iloc[:, [569, 578, 587, 596, 605, 614, 623, 631, 640, 649]]
+# print(leavingHome)
 #print("max median income: ", medianIncome.max()) = 157500
 #print('min median income; ', medianIncome.min()) = 4345
 scatterPlot["niceName"] = scatterPlot.iloc[:, 2]
@@ -21,6 +23,18 @@ scatterPlot["totalPeople"] = total
 scatterPlot["commuttersTakingTransit"] = numTransit
 scatterPlot["meanTravelTimeTransit"] = meanTravelTimeTransit
 scatterPlot["medianIncome"] = medianIncome
+#scatterPlot = pd.concat([scatterPlot, leavingHome], axis=1)
+#12am-5am = index 569
+#5am-5:30 = index 578 (+ 9)
+#5:30-6 = 587
+#6-6:30 = 596
+#6:30-7 = 605
+#7-7:30 = 614
+#7:30-8 = 623
+#8-8:30 = 631
+#8:30-9 = 640
+#9am-12pm = 649
+##tract data. want the time they leave in the morning, race, and industry they work in for transit riders
 
 
 scatterPlot.to_csv("scatterPlotData.csv")
